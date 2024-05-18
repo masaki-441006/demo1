75ecc6184b4428f8080233ba398fc70cb6d6a326
masaki-441006 <masaki.enjoy.0710@gmail.com>
2024-05-18 15:52:19 +0900

    0518

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -25,3 +25,45 @@ var integers5 = [1,2,3,4,5]
 integers5.remove(at:2)
 integers5.removeLast()
 integers5
+
+// 0518
+let array15 = [1,2,3,4,5,6]
+array15[3]
+array15.isEmpty
+array15.count
+array15.first
+array15.last
+
+//5章
+let value = 2
+if value <= 3 {
+    print("valueは3以下です")
+}
+
+let value1 = 1
+if value1 > 0 {
+    print("valueは0よりも大きい値です")
+}
+
+let value2 = 2
+if value2 <= 3 {
+    print("valueは3以下です")
+}else {
+    print("valueは3より大きいです")
+}
+let value3 = 2
+if value3 < 0 {
+    print("valu3は0以上3以下です")
+}
+else if value3 <= 3 {
+    print("valueは0以上3以下です")
+}else {
+    print("valueは３より大きいです")
+}
+
+let optionalA = Optional(1)
+if let a = optionalA {
+    print("あたいは\(a)です")}
+else {
+    "あたいが存在しません"
+}
